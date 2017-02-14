1ae384720653c1941fe157d507d9f8884771c14d
Kaz Matthews and Shruti Priya <kaz+shruti@>
2017-02-13 16:16:32 -0800

    add code for Words Not Starting With S, replace it with z

--- Objects/Translator.cs
+++ Objects/Translator.cs
@@ -4,15 +4,32 @@ namespace Leetspeak.Objects
   public class Translator
   {
     private string _phrase;
+    private string _userString;
 
     public Translator(string phrase)
     {
       _phrase = phrase;
+      _userString = phrase;
     }
     public string Translate()
     {
-      string userString = _phrase.Replace("e","3");
-      return userString;
+      string _userString = _phrase.Replace("e","3").Replace("E","3").Replace("o", "0").Replace("O","0").Replace("I", "1");
+
+      char[] chars = _userString.ToCharArray();
+      for(int i = 1; i < chars.Length; i++)
+      {
+        if (chars[i] == 's')
+        {
+          chars[i] = 'z';
+        }
+        else if (chars[i] == 'S')
+        {
+          chars[i] = 'Z';
+        }
+      }
+
+      _userString = string.Join("", chars);
+      return _userString;
     }
 
   }
--- Tests/LeetspeakTest.cs
+++ Tests/LeetspeakTest.cs
@@ -16,14 +16,44 @@ namespace Leetspeak
       Assert.Equal("attack", result);
     }
     [Fact]
-    public void Translate_ForWordsWitE_ReplaceWith3()
+    public void Translate_ForWordsWithE_ReplaceWith3()
     {
-      string phrase02 = "ear";
+      string phrase02 = "Ear";
       Translator newTranslator = new Translator(phrase02);
 
       string result = newTranslator.Translate();
 
       Assert.Equal("3ar", result);
+    }
+    [Fact]
+    public void Translate_ForWordsWithO_ReplaceWith0()
+    {
+      string phrase03 = "Out";
+      Translator newTranslator = new Translator(phrase03);
+
+      string result = newTranslator.Translate();
+
+      Assert.Equal("0ut", result);
+    }
+    [Fact]
+    public void Translate_ForWordsWithI_ReplaceWith1()
+    {
+      string phrase04 = "Initial";
+      Translator newTranslator = new Translator(phrase04);
+
+      string result = newTranslator.Translate();
+
+      Assert.Equal("1nitial", result);
+    }
+    [Fact]
+    public void Translate_ForWordsNotStartingWithS_ReplaceWithZ()
+    {
+      string phrase05 = "Styles";
+      Translator newTranslator = new Translator(phrase05);
+
+      string result = newTranslator.Translate();
+
+      Assert.Equal("Styl3z", result);
     }
   }
 }
